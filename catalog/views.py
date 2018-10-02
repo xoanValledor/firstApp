4e5066d7296200a7511e6388409fd3321aaff883
xoan <xoan@insulclock.com>
2018-10-02 22:46:14 +0200

    primer JSON enviado.

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,JsonResponse
+import json
 # Create your views here.
 def post_list(request):
-        return render(request, 'catalog/post_list.html', {})
+#         return render(request, 'catalog/post_list.html', {})
+#     obj = {"a": 1, "b": 2}
+#     return render(request, 'catalog/post_list.html', {"obj_as_json": json.dumps(obj)})
+#     return HttpResponse(json.dumps(obj), content_type='application/json')
+
+    data = {
+        'name': 'Pablo',
+        'location': 'MAdrid',
+        'is_active': True,
+        'pinchazos': 28,
+        
+         'MAC': '00:75:6C:00:34:C9',
+    }
+    return JsonResponse(data)
